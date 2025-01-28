ae1ca73579a305367b408258687b6c9821b16473
Werewolf-Alfa <vone2578biran@gmail.com>
2025-01-28 18:19:32 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="ru">
+
+<head>
+    <meta charset="UTF-8">
+<title>Медіа сай</title>
+
+  <link rel="shortcut icon" href="https://www.youtube.com/s/desktop/432433e9/img/favicon.ico" type="image/x-icon">
+
+
+</head>
+<style>
+
+
+
+.butt {
+margin-right: 0px;
+width: 250px;
+}
+
+</style>
+<body>
+<center>
+<h1>Медиа сайт</h1>
+<h2>Музика,Серіали та Іільми все наше</h2>
+<h3>Виберите категорию</h3>
+
+
+
+<a href="music/index.html">
+ <button class="butt">Музика
+<img width="220" height="250" src="img/music.jpeg">
+</button>
+</a>
+
+<a href="serial/index.html">
+ <button class="butt">Серіали
+<img width="220" height="250" src="img/serial.jpeg">
+</button>
+</a>
+
+
+<a href="films/index.html">
+ <button class="butt">Фільми
+<img width="220" height="250" src="img/film.jpeg">
+</button>
+</a>
+
+
+<p></p>
+<a href="map/map.html">
+ <button class="butt">
+  <p>Тестуваня страниці своєї карти</p>
+ </button>
+</a>
+
+
+
+
+
+
+
+</center>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
